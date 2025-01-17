0885fd3b13d0dac188a2a1aa485cefff490f812f
Esteban Canteros <estebanacanteros@gmail.com>
2025-01-17 18:10:16 -0300

    create order

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrderDto {}
+import { OrderStatus } from '@prisma/client';
+import { IsEnum, IsInt, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { OrderStatusList } from '../enum/order';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsInt()
+  totalItems: number;
+
+  @IsEnum(OrderStatusList, {message: `Possible status values are ${OrderStatusList}`})
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+}
